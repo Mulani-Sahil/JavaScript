953f928cd68e81f35df50bd9ba3bb99d846c89c6
Mulani Sahil <161868836+Mulani-Sahil@users.noreply.github.com>
2026-02-01 09:46:14 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=144553
+let accountEmail="sahil@goole.com"
+var accountPass="1234"
+accountCity="Pune"
+let accountState
+
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
+
+
+accountId=154
+accountEmail="sohail@gmail.com"
+accountPass="789"
+accountCity="Shrirampur"
+accountState="deactivated"
+
+
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
