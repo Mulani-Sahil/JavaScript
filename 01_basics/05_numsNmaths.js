2f432e7a94cc5f444f0647e047c806de4676d313
Mulani Sahil <161868836+Mulani-Sahil@users.noreply.github.com>
2026-02-12 04:41:25 +0000

    playing with numbers and maths

---
+++ 01_basics/05_numsNmaths.js
@@ -0,0 +1,12 @@
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.min(4,3,6,5,4,8,9,6,3,2,2));
+// console.log(Math.max(4,3,6,5,4,8,9,6,3,2,2));
+// console.log((Math.random()*10 )+ 1);
+// console.log(Math.random());  //values between 0 and 1
+const min = 10
+const max =20
+console.log(Math.floor((Math.random()*(max-min+1)))+min);
